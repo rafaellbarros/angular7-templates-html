b9c884c269953f77b0aaebfa7084838ff16c90dc
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-08 14:16:12 -0300

    :construction: ngSwitch

--- src/app/models/employees.ts
+++ src/app/models/employees.ts
@@ -3,10 +3,14 @@ export interface Employee {
     salary: number;
     bonus: number;
 }
+
 const employees: Employee[] = [
-  {name: 'Fulano da Silva', salary: 2000, bonus: 0},
-  {name: 'Cicrano da Silva', salary: 15000, bonus: 0},
+  {name: 'Fulano da Silva', salary: 1000, bonus: 0},
+  {name: 'Cicrano da Silva', salary: 10000, bonus: 0},
   {name: 'Beltrano da Silva', salary: 900, bonus: 100},
+  {name: 'Funcionario X', salary: 800, bonus: 10},
+  {name: 'Funcionario Y', salary: 700, bonus: 100},
+  {name: 'Funcionario Z', salary: 600, bonus: 1000},
 ];
 
 export default employees;
