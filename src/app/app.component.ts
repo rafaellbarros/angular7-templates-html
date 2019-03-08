9e3f2be5c885439190b16e4619dacaf0d6261e9d
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-08 15:08:12 -0300

    :construction: Iniciando com ngTemplateOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'iniciando-angular2-mais';
+export class AppComponent implements OnInit{
+  title = 'app';
+  flagLogo = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.flagLogo = false;
+    }, 5000);
+  }
 }
